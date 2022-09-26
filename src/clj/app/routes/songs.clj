9f48ca18c3f45e07bcdd6ffab705807591842e84
Casey Link <unnamedrambler@gmail.com>
2022-09-26 13:08:17 +0200

    Live search songs list

--- src/clj/app/routes/songs.clj
+++ src/clj/app/routes/songs.clj
@@ -3,7 +3,8 @@
    [app.render :as render]
    [app.icons :as icon]
    [ctmx.core :as ctmx]
-   [tick.core :as t]))
+   [tick.core :as t]
+   [clojure.string :as clojure.string]))
 (defn bool-bubble [is-active]
   [:span {:class
           (render/cs "px-2 inline-flex text-xs leading-5 font-semibold rounded-full"
@@ -41,10 +42,12 @@
          (datetime last-played)]]]]]))
 
 (defn song-list [songs]
-  [:ul {:role "list", :class "divide-y divide-gray-200"}
-   (map (fn [song]
-          [:li
-           (song-row song)]) songs)])
+  (if (empty? songs)
+    "Songs not found"
+    [:ul {:role "list", :class "divide-y divide-gray-200"}
+     (map (fn [song]
+            [:li
+             (song-row song)]) songs)]))
 
 (def _songs [{:song/title "Hooray"
               :song/score 0
@@ -57,10 +60,42 @@
               :song/last-played (t/today)
               :song/active true}])
 
+(defn norm [s]
+  (-> s
+      (clojure.string/upper-case)
+      (clojure.string/trim)))
+
+(defn search-songs [keyword all-songs]
+  (filter (fn [s]
+            (clojure.string/includes?
+             (norm (:song/title s))
+             (norm keyword))) all-songs))
+
+(ctmx/defcomponent ^:endpoint songs-filter [req]
+  [:div {:class "relative rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:border-indigo-600 focus-within:ring-1 focus-within:ring-indigo-600"}
+   [:label {:for "song", :class "absolute -top-2 left-2 -mt-px inline-block bg-white px-1 text-xs font-medium text-gray-900"}
+    "Song"]
+   [:input {:type "text"
+            :name "song"
+            :id "song"
+            :class "block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+            :placeholder "Watermelon Man"
+            :hx-get "songs-list"
+            :hx-trigger "keyup changed delay:500ms"
+            :hx-target "#songs-list"}]])
+
+(ctmx/defcomponent ^:endpoint songs-list [req song]
+  (let [filtered-songs (search-songs song _songs)]
+
+    [:div {:class "overflow-hidden bg-white shadow sm:rounded-md"
+           :id "songs-list"}
+     (song-list filtered-songs)]))
+
 (defn songs-routes []
   (ctmx/make-routes
    "/songs"
    (fn [req]
      (render/html5-response
-      [:div {:class "overflow-hidden bg-white shadow sm:rounded-md"}
-       (song-list _songs)]))))
+      [:div {:class "mt-6"}
+       (songs-filter req)
+       (songs-list req "")]))))
