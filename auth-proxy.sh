c99593c93f744037079d70eb20a3f2727723c02b
Casey Link <unnamedrambler@gmail.com>
2022-11-15 16:50:59 +0100

    Increase auth cookie timeout

--- auth-proxy.sh
+++ auth-proxy.sh
@@ -16,13 +16,9 @@
       --cookie-secret="***REMOVED***" \
       --cookie-csrf-per-request=true \
       --cookie-refresh="1m" \
-      --cookie-expire="30m" \
+      --cookie-expire="720m" \
       --skip-provider-button=true \
       --pass-host-header=true \
       --pass-user-headers=true \
       --pass-access-token=true \
       --upstream=http://localhost:6161
-
-# -----BEGIN PUBLIC KEY-----
-# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt8vGXMaB+9vgj4tAfCKrGlX+NW7A2Dvj0G0rKocF2yofza6QnaGhNRhglXaLlwXUb4ANHCXOds0bzhmJRmQLAJ0o8ReUiyblExZ8dXN7duZzONKTmRBNAZOOkYMm8yU17/NYqaomNJ7aJh0GamXg4NgXRhS3NNiUrrT4+j46Vi2ldwGqB6YDc2DM3QH/tOpFx/xT7t2RKWSUXGdamRKcghuUFwpOfESWnIDBXqiwrbzT8RyPPqHEOUNugIzsrPoPUQdxzPMvMcihBq1hKJozB+xnIt9U5hMxqAygldfIdZBcIX/ffox+ZK2SJbv/pE6O12t0QtlaKX1V5NWgFPa8RQIDAQAB
-# -----END PUBLIC KEY-----
