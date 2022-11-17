7f311bcbaf1430e91c06e55465e16e740084bd4b
Casey Link <unnamedrambler@gmail.com>
2022-11-17 08:47:50 +0100

    Cleanup dependencies

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ((nil . ((cider-clojure-cli-global-options     . "")
-         (cider-default-cljs-repl              . custom)
          (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
          (cider-repl-display-help-banner       . nil)
