d7a6efa256a14de372a2123c4a2d58171c2b255f
Casey <unnamedrambler@gmail.com>
2023-01-24 13:33:49 +0100

    Add tempura edn <-> po/pot converter

--- src/clj/app/i18n.clj
+++ src/clj/app/i18n.clj
@@ -1,8 +1,13 @@
 (ns app.i18n
-  (:require [clojure.set      :refer [intersection]]
-            [clojure.java.io :as clojure.java.io]
-            [taoensso.tempura :as tempura :refer [tr]]
-            [taoensso.encore  :as enc]))
+  (:require
+   [clojure.pprint :as clojure.pprint]
+   [clojure.edn :as edn]
+   [medley.core :as m]
+   [clojure.string :as str]
+   [clojure.set      :refer [intersection]]
+   [clojure.java.io :as clojure.java.io]
+   [taoensso.tempura :as tempura :refer [tr]]
+   [taoensso.encore  :as enc]))
 
 (def default-locale :en)
 
@@ -51,3 +56,151 @@
 
 (defn tr-from-req [req]
   (:tempura/tr req))
+
+(def ^:private remove-empty-lines
+  "Transducer that remove groups of empty lines."
+  (filter #(not= "" (first %))))
+
+(def ^:private split-on-blank
+  "Transducer that splits on blank lines."
+  (partition-by #(= % "")))
+
+(declare parse-comments)
+(declare parse-keys)
+
+(def ^:private parse-entries
+  (let [comment-line? (fn [line] (str/starts-with? line "#"))]
+    (map (fn [lines]
+           (let [[comments keys] (partition-by comment-line? lines)]
+             {:comments (parse-comments comments)
+              :keys     (parse-keys keys)})))))
+
+(defn- parse-comments [comments]
+  (into {}
+        (for [comment comments]
+          (let [len          (count comment)
+                proper?      (>= len 2)
+                start        (when proper? (subs comment 0 2))
+                rest         (when proper? (subs comment 2))
+                remove-empty #(filter (partial not= "") %)]
+            (case start
+              "#:" [:reference (remove-empty (str/split rest #" +"))]
+              "#," [:flags (remove-empty (str/split rest #" +"))]
+              "# " [:translator-comment rest]
+              ;; TODO: add other types
+              [:unknown-comment comment])))))
+
+(defn- join-sequential-strings [rf]
+  (let [acc (volatile! nil)]
+    (fn
+      ([] (rf))
+      ([res] (if-let [a @acc]
+               (do (vreset! acc nil)
+                   (rf res (apply str a)))
+               (rf res)))
+      ([res i]
+       (if (string? i)
+         (do (vswap! acc conj i)
+             res)
+         (rf (or (when-let [a @acc]
+                   (vreset! acc nil)
+                   (rf res (apply str a)))
+                 res)
+             i))))))
+
+(def ^:private keywordize-things
+  (map #(if (string? %) % (keyword %))))
+
+(defn- parse-keys [keys]
+  (apply hash-map
+         (transduce (comp join-sequential-strings
+                          keywordize-things)
+                    conj
+                    []
+                    ;; XXX: double hack for double fun!
+                    (edn/read-string (str "[" (apply str (interpose " " keys)) "]")))))
+
+(def ^:private parser
+  (comp split-on-blank
+        remove-empty-lines
+        parse-entries))
+
+(defn parse
+  "Parse the PO file given as stream of lines `l`."
+  [l]
+  (transduce parser conj [] l))
+
+(defn parse-from-reader
+  "Parse the PO file given in reader `rdr`.  `rdr` must implement `java.io.BufferedReader`."
+  [rdr]
+  (parse (line-seq rdr)))
+
+(defn parse-from-string
+  "Parse the PO file given as string."
+  [s]
+  (parse (str/split-lines s)))
+
+(defn ->po-entry [[key untranslated-str]]
+  (format "#: %s
+#, ycp-format
+msgctxt \"%s\"
+msgid \"%s\"
+msgstr \"\"" (str key) (str key) untranslated-str))
+
+(def pot-header
+  "msgid \"\"
+msgstr \"\"
+Content-Type: text/plain; charset=UTF-8
+")
+
+(defn locale->seq
+  "Convert a tempura locale map into a list of key/string tuples"
+  [m]
+  (->> m
+       (reduce (fn [acc [k sub-m]]
+                 (concat acc
+                         (if (map? sub-m)
+                           (map (fn [[sub-k string]]
+                                  [(keyword (name k) (name sub-k)) string]) sub-m)
+                           [[k sub-m]])))
+               [])
+       ;;  Cannot have blank msgids
+       (filter (fn [[k untranslated-str]]
+                 (not (str/blank? untranslated-str))))))
+
+(defn gen-pot [fname dname]
+  (let [m (load-resource fname)
+        entries (map ->po-entry (locale->seq m))]
+    (spit (str "resources/" dname)
+          (str pot-header
+               (str/join "\n" entries)))))
+
+(defn convert-po [locale]
+  (let [fname (str "resources/lang/" locale ".po")
+        dname (str "resources/lang/" locale ".edn")
+        fallback-m (load-resource "lang/en.edn")
+        po-contents (slurp fname)
+        parsed (parse-from-string po-contents)
+        translated-map (->> parsed
+                            (reduce (fn [acc {:keys [comments keys]}]
+                                      (let [{:keys [msgid msgstr msgctxt]} keys]
+                                        (if msgid
+                                          (let [kw (edn/read-string msgctxt)
+                                                ns (keyword (namespace kw))
+                                                n (keyword (name kw))
+                                                path (if ns [ns n] [n])
+                                                finalstr (if-not (str/blank? msgstr) msgstr (get-in fallback-m path))]
+                                            (assoc-in acc path finalstr))
+                                          acc)))
+
+                                    {}))]
+    (clojure.pprint/pprint translated-map (clojure.java.io/writer dname))))
+
+(comment
+  ;; create pot
+  (gen-pot "lang/en.edn" "lang/app.pot")
+  ;; convert translated po to edn
+  (convert-po "de") ;; rcf
+
+  ;;
+  )
