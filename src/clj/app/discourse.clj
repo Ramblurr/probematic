24e54b25c7c92da26f4b1fee672763c31fe6bab5
Casey <unnamedrambler@gmail.com>
2023-01-30 14:53:43 +0100

    Use Discourse API to create posts for gigs

--- src/clj/app/discourse.clj
+++ src/clj/app/discourse.clj
@@ -1,5 +1,7 @@
 (ns app.discourse
   (:require
+   [org.httpkit.client :as client]
+   [selmer.parser :as selmer]
    [app.datomic :as d]
    [clojure.set :as set]
    [clojure.string :as str]
@@ -7,12 +9,19 @@
    [integrant.repl.state :as state]
    [jsonista.core :as j]
    [martian.core :as martian]
-   [martian.httpkit :as martian-http]))
+   [martian.httpkit :as martian-http]
+   [app.urls :as url]
+   [app.ui :as ui]
+   [app.queries :as q]
+   [app.debug :as debug]
+   [medley.core :as m]
+   [app.gigs.domain :as domain]))
 
 (defn add-authentication-header [api-key username]
   {:name ::add-authentication-header
    :enter (fn [ctx]
             (-> ctx
+                (assoc-in [:request :headers "Content-Type"] "multipart/form-data;")
                 (assoc-in [:request :headers "Api-Key"] api-key)
                 (assoc-in [:request :headers "Api-Username"] username)))})
 
@@ -24,7 +33,6 @@
     (if (= 200 status)
       (j/read-value body j/keyword-keys-object-mapper)
       {:error r})))
-
 (defn sync-avatars! [{:keys [env conn]}]
   (let [db (datomic/db conn)
         {:keys [api-key username forum-url]} (:discourse env)
@@ -43,52 +51,176 @@
                  (map #(update % :id str))
                  (map #(set/rename-keys % {:avatar_template :member/avatar-template :id :member/discourse-id :username :member/nick})))]
     (d/transact conn {:tx-data txs})))
+(defn wrap-auth [req {:keys [discourse] :as env}]
+  (-> req
+      (assoc-in [:headers "Api-Key"] (:api-key discourse))
+      (assoc-in [:headers "Api-Username"] (:username discourse))))
+
+(defn wrap-api-url [req {:keys [discourse]}]
+  (update-in req [:url] (fn [path] (str (:forum-url discourse) path))))
+
+(defn request! [env req]
+  (let [{:keys [status body] :as r} @(client/request
+                                      (-> req
+                                          (wrap-auth env)
+                                          (wrap-api-url env)))]
+
+    (if (= 200 status)
+      (j/read-value body j/keyword-keys-object-mapper)
+      (throw (ex-info "Discourse Error" {:resp r})))))
+
+(defn markdown-quote [v]
+  (when v
+    (->> (str/split-lines v)
+         (map (fn [line]
+                (str "> " line)))
+         (str/join "\n"))))
+
+(defn gig->markdown-post [env {:gig/keys [gig-type attendance-summary status planned-songs title more-details location contact leader date end-date] :as gig}]
+  (selmer/render
+   "**Bestätigt: {{confirmed}}**
+
+
+**Datum:** {{time}} {{date}}
+
+**Ort:** {{location}}
+
+{% if not details|empty? %}**Programm:**
+{{details|safe}}{% endif %}
+
+{% if not leader|empty? %}**Gig-Master:** {{leader}}{% endif %}
+
+**Attendance**:
+
+{% for item in counts %}{{item.icon}} {{item.value}}
+{% endfor %}
+
+**{{planned-songs-title}}**:
+
+{% for item in planned-songs %}{{item.position}}. [{{item.label}}]({{item.href}}) {% if item.extra %}{{item.extra}}{% endif %}
+{% endfor %}
+
+
+[**:arrow_right: More info in the Probematic**]({{probematic-link}})
+
+"
+   {:confirmed
+    (case status
+      :gig.status/confirmed ":gig_confirmed:"
+      :gig.status/unconfirmed ":gig_unconfirmed:"
+      :gig.status/cancelled ":gig_cancelled:")
+    :probematic-link (url/absolute-link-gig env (:gig/gig-id gig))
+    :date "" #_(ui/gig-date-plain gig)
+    :time (ui/gig-time gig)
+    :location location
+    :details (markdown-quote more-details)
+    :leader leader
+    :planned-songs (debug/xxx planned-songs)
+    :planned-songs-title (if (domain/setlist-gig? gig) "Setlist" "Probeplan")
+    :counts (map (fn [[plan count]]
+                   {:icon
+                    (get
+                     {:plan/no-response "**—**"
+                      :plan/definitely ":gruener_kreis:"
+                      :plan/probably ":gruener_ringel:"
+                      :plan/unknown ":fragezeichen:"
+                      :plan/probably-not ":roter_quadratischer_umriss:"
+                      :plan/definitely-not ":rotes_quadrat:"
+                      :plan/not-interested ":schwarz_kreuz:"} plan)
+                    :value (str count)})
+
+                 attendance-summary)}))
+
+(defn topic-for-gig [{:keys [env]} gig]
+  (try
+    (request! env
+              {:method :get
+               :url (format "/t/external_id/%s.json" (:gig/gig-id gig))})
+    (catch Throwable e
+      (if (= 404 (-> (ex-data e) :resp :status))
+        nil
+        (throw e)))))
+
+(defn first-post-for-topic [topic]
+  (m/find-first (fn [{:keys [post_number]}]
+                  (= 1 post_number))
+                (-> topic
+                    :post_stream
+                    :posts)))
+
+(defn update-post-for-gig [env gig post-id]
+  (request! env
+            {:method :put
+             :url (format "/posts/%s.json" post-id)
+             :headers {"content-type" "application/json"}
+             :body (j/write-value-as-string
+                    {:raw (gig->markdown-post env gig)
+                     :edit_reason "something changed in probematic"})})
+  nil)
+
+(defn new-thread-for-gig
+  "Creates a new topic for the gig, returns the topic id."
+  [env gig]
+  (let [gig-id (:gig/gig-id gig)]
+    (:id
+     (request! env
+               {:method :post
+                :url "/posts.json"
+                :form-params {:title (str (:gig/title gig) " " (ui/gig-date-plain gig))
+                              :raw (gig->markdown-post env gig)
+                              :category 4
+                              ;; :skip_validations true
+                              ;; :auto_track false
+                              ;; :embed_url (str (url/absolute-link-gig env gig-id) "#")
+                              :external_id gig-id}}))))
+
+(defn summarize-attendance [gig db]
+  (assoc gig :gig/attendance-summary
+         (->> (q/attendances-for-gig db (:gig/gig-id gig))
+              (group-by :attendance/plan)
+              (m/map-vals count))))
+
+(defn planned-songs [gig {:keys [env db]}]
+  (assoc gig :gig/planned-songs
+         (map (fn [{:song/keys [title song-id] :keys [emphasis position]}]
+                {:href (url/absolute-link-song env song-id)
+                 :label title
+                 :position (inc position)
+                 :extra (when (= :probeplan.emphasis/intensive emphasis) (str " (intensive)"))})
+              (q/planned-songs-for-gig db (:gig/gig-id gig)))))
+
+(defn upsert-thread-for-gig
+  "If a thread was created, returns the topic id, otherwise nil."
+  [{:keys [db env] :as sys} gig]
+  (let [gig (-> gig
+                (summarize-attendance db)
+                (planned-songs sys))]
+    (if-let [post-id (:id (first-post-for-topic (topic-for-gig {:env env} gig)))]
+      (update-post-for-gig env gig post-id)
+      (new-thread-for-gig env gig))))
 
 (comment
-  (martian/explore m)
-  (martian/explore m :admin-list-users)
 
   (do
     (require '[integrant.repl.state :as state])
     (def conn (-> state/system :app.ig/datomic-db :conn))
     (def env (-> state/system :app.ig/env))
+    (def db  (datomic/db conn)) ;; rcf
+    (let [{:keys [api-key username forum-url]} (:discourse env)]
+      (def m (martian-http/bootstrap-openapi url-discourse-open-api {:server-url "https://forum.streetnoise.at"
+                                                                     :interceptors (concat martian/default-interceptors
+                                                                                           [(add-authentication-header api-key username)]
+                                                                                           [martian-http/perform-request])})))) ;; rcf
 
-    (sync-avatars! {:conn conn :env env})) ;; rcf
-
-  (do
-
-    (def m (martian-http/bootstrap-openapi url-discourse-open-api {:server-url "https://forum.streetnoise.at"
-                                                                   :interceptors (concat martian/default-interceptors
-                                                                                         [(add-authentication-header api-key username)]
-                                                                                         [martian-http/perform-request])}))
-
-    (def user-list
-      (->>
-       (list-users m)))
-
-    (def members
-      (->>
-       (d/find-all db :member/gigo-key [:member/name :member/email :member/gigo-key])
-       (map first)
-       (map #(update % :member/email str/lower-case))))
-
-    (def joined (set/join user-list members {:email :member/email}))
-    (count user-list)
-    (count members)
-    (count joined)
-
-    (def txs
-      (->> joined
-           (map #(select-keys % [:member/gigo-key :avatar_template :id :username]))
-           (map #(update % :id str))
-           (map #(set/rename-keys % {:avatar_template :member/avatar-template :id :member/discourse-id :username :member/nick}))))
-
-    (d/transact conn {:tx-data txs})) ;; add avatar txs
-
-  (map :email user-list)
-  (map :member/email members)
-
+  (sort-by first (martian/explore m))
+  (martian/explore m :admin-list-users)
   (d/find-all db :member/gigo-key [:member/name :member/email :member/gigo-key :member/avatar-template :member/discourse-id :member/nick])
+
+  (def g (q/retrieve-gig db "0185cfb2-4ac4-8cab-b1d9-5dd1fd1a3e20"))
+  (def g2 (q/retrieve-gig db "ag1zfmdpZy1vLW1hdGljcjMLEgRCYW5kIghiYW5kX2tleQwLEgRCYW5kGICAgMD9ycwLDAsSA0dpZxiAgMCil42RCQw"))
+  (:id (first-post-for-topic (topic-for-gig {:env env} g)))
+
+  (upsert-thread-for-gig {:env env :db db} g2) ;; rcf
 
   ;;
   )
